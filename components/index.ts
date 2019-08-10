96475c9d036d6eb10ccfde7d16c5bf80e6debcdb
freevue <hackish@hanmail.net>
2019-08-10 13:20:18 +0900

    [버그] typescript .vue 버그 수정

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,3 @@
-export { default as MainLogo } from './MainLogo';
-export { default as Card } from './Card';
-export { default as AppBar } from './AppBar';
+export { default as MainLogo } from './MainLogo.vue';
+export { default as Card } from './Card.vue';
+export { default as AppBar } from './AppBar.vue';
--- containers/Main/index.ts
+++ containers/Main/index.ts
@@ -1,1 +1,1 @@
-export { default as MainPopList } from './PopList';
+export { default as MainPopList } from './PopList.vue';
