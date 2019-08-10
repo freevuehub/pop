636763341f10e2d6fb28e6cb2be5826301513c85
freevue <hackish@hanmail.net>
2019-08-10 13:35:37 +0900

    [head] viewport 설정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default {
     title: 'Freevue - POP',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, user-scalable=no, initial-scale=1.0' },
+      { name: 'viewport', content: 'width=device-width, user-scalable=no, initial-scale=1' },
       { name: 'theme-color', content: '#72cc96' },
       {
         hid: 'description',
